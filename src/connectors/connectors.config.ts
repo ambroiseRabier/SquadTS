1841abbcad34ac0267577b39eb0b1a7f0b330c59
Ambroise Rabier <me@ambroise-rabier.dev>
2025-03-03 22:35:29 +0100

    - Checking package-lock.json in pre-commit. - Better describe for Discord token.

--- src/connectors/connectors.config.ts
+++ src/connectors/connectors.config.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+const discordTokenRegex = /^[a-zA-Z0-9_\-]{24,}\.[a-zA-Z0-9_\-]{6,}\.[a-zA-Z0-9_\-]{27,}$/;
+
 export const connectorsOptionsSchema = z
   .object({
     discord: z.discriminatedUnion('enabled', [
@@ -10,7 +12,15 @@ export const connectorsOptionsSchema = z
         token: z
           .string()
           .optional()
-          .describe('Discord token. Get at https://discord.com/developers/applications'),
+          .refine(token => token && token.match(discordTokenRegex), 'It appears you are not using a Discord bot token.')
+          .describe(
+            'Discord bot token. Do not confuse with public key.\n' +
+            'You can reset the token:\n' +
+            '1. Go to https://discord.com/developers/applications\n' +
+            '2. Select your bot.\n' +
+            '3. Click on the bot menu and click on "Reset Token".\n' +
+            '4. Copy the new token.'
+          ),
       }),
       z.object({
         enabled: z.literal(true),
